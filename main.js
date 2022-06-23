d63951fa233c95977e68c9993a678b4276eb4081
Monday-Obazee <mondayobazee@gmail.com>
2022-06-23 21:52:04 +0100

    Printed my Name and hieght

---
+++ main.js
@@ -0,0 +1,24 @@
+let fName = "Monday";
+let lName = "Obazee";
+let hight = 6;
+let hNum = "ft";
+let country = "Nigeria";
+
+console.log(
+  "My name is" +
+    " " +
+    fName +
+    " " +
+    lName +
+    "," +
+    "I am" +
+    " " +
+    6 +
+    "ft" +
+    " " +
+    "tall" +
+    " " +
+    "and I am from" +
+    " " +
+    country
+);
